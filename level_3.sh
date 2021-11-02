679b12d47ddb63e35d814af24f05da61d7d79ea9
tyler <baile320@umn.edu>
2021-11-02 11:54:23 -0500

    do level 3

---
+++ level_3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit3.html
+
+# password = UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK, found by `cat ./spaces\ in\ this\ filename`
+ssh bandit3@bandit.labs.overthewire.org -p 2220
