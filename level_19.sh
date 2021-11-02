801a4c6b1e9401e54fe42a0a056361aebf479062
tyler <baile320@umn.edu>
2021-11-02 16:11:53 -0500

    do level 19

---
+++ level_19.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# https://overthewire.org/wargames/bandit/bandit19.html
+
+# to find password:
+# `ssh bandit18@bandit.labs.overthewire.org -p 2220 "cat readme"`
+# password: IueksS7Ubh8G3DCwVzrTd8rAVOwq3M5x
+
+ssh bandit19@bandit.labs.overthewire.org -p 2220
