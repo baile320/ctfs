d0422f2c092eee95773dacd7381b166a0db85c7f
tyler <baile320@umn.edu>
2021-11-02 11:53:12 -0500

    do level 2

---
+++ level_2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit2.html
+
+# password = CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9, found by viewing `cat /home/bandit1/-`
+
+ssh bandit2@bandit.labs.overthewire.org -p 2220
