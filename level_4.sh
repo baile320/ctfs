c8fb12fb153ad5cef10e2d8cc24b17947d1b7df3
tyler <baile320@umn.edu>
2021-11-02 11:54:28 -0500

    do level 4

---
+++ level_4.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit4.html
+
+# password = pIwrPrtPN36QITSp3EQaw936yaFoFgAB, found by `cat ~/inhere/.hidden`
+ssh bandit4@bandit.labs.overthewire.org -p 2220
