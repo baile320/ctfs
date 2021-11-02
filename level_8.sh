0061bcbf3e4fe918dd3c1feb4a4ee4bb21e94da3
tyler <baile320@umn.edu>
2021-11-02 11:54:49 -0500

    do level 8

---
+++ level_8.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit8.html
+
+# password = cvX2JJa4CFALtqS87jk27qwqGhBM9plV, found by
+# `cat data.txt | grep millionth`
+
+ssh bandit8@bandit.labs.overthewire.org -p 2220
