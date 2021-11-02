5bde21e94c63c6b5777445dbfa91ed3096f12445
tyler <baile320@umn.edu>
2021-11-02 12:02:06 -0500

    fix level_10.sh

--- level_10.sh
+++ level_10.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# https://overthewire.org/wargames/bandit/bandit9.html
+# https://overthewire.org/wargames/bandit/bandit10.html
 
 # password = truKLdjsbJ5g7yyJ2X2R0o3a5HQJFuLk, found by
 # `strings data.txt | grep ===` # note, there are lots of equals signs so you can try `grep ==` or `grep ====` etc.
 
-ssh bandit9@bandit.labs.overthewire.org -p 2220
+ssh bandit10@bandit.labs.overthewire.org -p 2220
