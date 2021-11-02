8b7fdb7547a0d290073bb8b76e499a15ad49022f
tyler <baile320@umn.edu>
2021-11-02 11:54:32 -0500

    do level 5

---
+++ level_5.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit5.html
+
+# password = koReBOKuIDDepwhWk7jZC0RTdopnAYKh, found by
+# `cd ~/inhere`
+# `file ./*` # notice that ./-file07 has ASCII text
+# `cat ./-file07`
+
+ssh bandit5@bandit.labs.overthewire.org -p 2220
