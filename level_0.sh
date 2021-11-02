6eb7fabb248b1cdc134e9511fe6f1f5a9c20d093
tyler <baile320@umn.edu>
2021-11-02 11:52:59 -0500

    do level 0 and 1

---
+++ level_0.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit0.html
+
+# log in w/ username/password bandit0
+ssh bandit0@bandit.labs.overthewire.org -p 2220
---
+++ level_1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://overthewire.org/wargames/bandit/bandit1.html
+
+# password = boJ9jbbUNNfktd78OOpsqOltutMc3MY1, `cat /home/bandit0/readme`
+ssh bandit1@bandit.labs.overthewire.org -p 2220
