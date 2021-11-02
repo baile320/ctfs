933f5818990721da37dd240251a4b42c496b46e2
tyler <baile320@umn.edu>
2021-11-02 12:21:07 -0500

    do level 11

---
+++ level_11.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# https://overthewire.org/wargames/bandit/bandit11.html
+
+# password = IFukwKGsFW8MOq3IRFqrxE1hxTNEbUPR, found by
+# `base64 -d data.txt`
+
+ssh bandit11@bandit.labs.overthewire.org -p 2220
