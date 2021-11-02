81260e37296989dc870dbefa95c912e0cb80ce42
tyler <baile320@umn.edu>
2021-11-02 16:12:02 -0500

    do level 20

---
+++ level_20.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# https://overthewire.org/wargames/bandit/bandit20.html
+
+# to find password:
+# `./bandit20-do cat /etc/bandit_pass/bandit20`
+# password: GbKksEFF4yrVs6il55v6gwY5aVje5f0j
+
+ssh bandit20@bandit.labs.overthewire.org -p 2220
