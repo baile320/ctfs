a517edb07f9ffb68634c52458cc1b8741d24a9e0
tyler <baile320@umn.edu>
2021-11-03 13:55:01 -0500

    add .sh files for 30 and 31

---
+++ bandit/level_30.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# https://overthewire.org/wargames/bandit/bandit30.html
+
+# password: 5b90576bedb2cc04c86a9e924ce42faf
+
+ssh bandit30@bandit.labs.overthewire.org -p 2220
---
+++ bandit/level_31.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# https://overthewire.org/wargames/bandit/bandit31.html
+
+# password: 47e603bb428404d265f59c42920d81e5
+
+ssh bandit31@bandit.labs.overthewire.org -p 2220
